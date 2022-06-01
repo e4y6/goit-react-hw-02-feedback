eda159d3113ae758067115fd6e0c758db5175b1d
e4y6 <99596168+e4y6@users.noreply.github.com>
2022-06-01 14:56:31 +0300

    button cursor-pointer added

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -22,6 +22,7 @@ const Button = styled.button`
   margin: 0 15px;
   height: 45px;
   width: 80px;
+  cursor: pointer;
   border: 1px solid #3476ff;
   border-radius: 45%;
   background-color: #ff12d7;
